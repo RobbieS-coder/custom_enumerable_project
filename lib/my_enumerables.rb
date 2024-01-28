64dd8aaa66604fa0d836c2ab00e292a30a1fa22b
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-01-28 12:07:00 +0000

    Add my_map

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -40,12 +40,16 @@ module Enumerable
 
     count
   end
+
+  def my_map
+    results = []
+
+    my_each { |elem| results.push(yield elem) }
+
+    results
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   def my_each
     for elem in self
